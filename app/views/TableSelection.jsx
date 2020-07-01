98c456cfb12cc1881c2bacad0e09b92c384987f5
Norman Argueta <uchiha.norman@gmail.com>
2020-06-30 22:17:06 -0600

    Added react navigation

---
+++ app/views/TableSelection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TableSelection = () => {
+  return (
+    <View>
+      <Text>Tables screen</Text>
+    </View>
+  );
+};
+
+export default TableSelection;
