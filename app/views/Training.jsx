408d3fef943efcd4f67ee16538d87f4065be25e1
Norman Argueta <uchiha.norman@gmail.com>
2020-08-12 00:00:18 -0600

    Added logic in training screen to verify if answer is correct

--- app/views/Training.jsx
+++ app/views/Training.jsx
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput, Text, Button } from 'react-native';
 
 import Styles from '../styles/Styles';
 import MultiplierText from '../components/MultiplierText';
+import AnswerModal from '../components/AnswerModal';
 
 export default Training = ({ route }) => {
+  const table = route.params.table;
   const [index, setIndex] = useState(0);
+  const [answer, setAnswer] = useState('');
+  const [tableIndexs, setTableIndexs] = useState([1, 2, 3, 4, 5, 6, 7, 8, 8, 10]);
+  const [modalVisible, setModalVisibility] = useState(false);
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState(false);
 
-  const getRandomIndex = (arr) => arr[Math.floor((Math.random()*arr.length))];
+  const getRandomIndex = () => {
+    const tables = tableIndexs;
+    const randomIndex = Math.floor((Math.random() * tables.length))
+    const randomValue = tables[randomIndex];
+    tables.splice(randomIndex, 1);
+    setTableIndexs(tables);
 
-  let tables = [];
-  for (let i = 1; i <= 10; i++) {
-    tables.push(i);
+    return randomValue;
+  };
+
+  const verifyAnswer = () => {
+    console.log(tableIndexs);
+    if (tableIndexs.length > 0) {
+      console.log('answer', answer);
+      console.log('result', table * index == parseInt(answer, 10));
+      setIsCorrectAnswer(table * index == parseInt(answer, 10));
+      setModalVisibility(true);
+      setIndex(getRandomIndex());
+    }
   };
 
   useEffect(() => {
-    setIndex(getRandomIndex(tables));
+    setIndex(getRandomIndex());
   }, [])
 
   return (
@@ -23,10 +43,28 @@ export default Training = ({ route }) => {
       <View style={customStyles.cardContainer}>
         <View style={ customStyles.card }>
           <MultiplierText
-            tableNumber={route.params.table}
+            tableNumber={table}
             index={index}
           />
         </View>
+        <View>
+          <Text>Respuesta</Text>
+          <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={text => setAnswer(text)}
+            value={answer}
+            keyboardType={'numeric'}
+          />
+          <Button
+            title="Verificar"
+            onPress={() => verifyAnswer()}
+          />
+        </View>
+        <AnswerModal
+          visible={modalVisible}
+          setVisibility={setModalVisibility}
+          correctAnswer={isCorrectAnswer}
+        />
       </View>
     </View>
   );
@@ -37,10 +75,9 @@ const customStyles = StyleSheet.create({
     flex: 1,
     alignContent: 'center',
     justifyContent: 'center',
-    flexDirection: 'row'
+    flexDirection: 'column'
   },
   card: {
-    flex: 1,
     borderWidth: 2,
     borderColor: "#000000",
     borderRadius: 6,
